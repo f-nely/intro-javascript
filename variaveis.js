be9b58d205f4d3b4caa25acce2902274adf6b6b4
nely de faria <nelly.nf54@gmail.com>
2022-08-01 18:18:59 -0300

    Cria variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+console.log('Trabalhando com variáveis: ');
+
+let primeiroNome = 'Ricardo';
+let sobrenome = 'Bugan';
+let idade = 29;
+
+console.log(primeiroNome, sobrenome); // Ricardo Bugan
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`); //Meu nome é Ricardo Bugan
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto); // RicardoBugan
