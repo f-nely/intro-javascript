1a90af471a67b1dd558619d06afb216a2875e9b7
nely de faria <nelly.nf54@gmail.com>
2022-08-01 18:19:28 -0300

    Cria conversao-tipo.js

---
+++ conversao-tipo.js
@@ -0,0 +1,8 @@
+console.log('Conversão de Tipos: ');
+
+console.log('ano ' + 2020); // ano 2020
+console.log('2' + '2'); // 22
+console.log(parseInt('2') + parseInt('2')); // 4
+console.log('10' / '2'); // 5
+console.log('Ricardo' / 2); // NAN
+console.log(6.5 / 2); // 3.25
