2045c6dd58c41302f8155710141b34750253d194
nely de faria <nelly.nf54@gmail.com>
2022-08-01 18:20:50 -0300

    Cria listas.js

---
+++ listas.js
@@ -0,0 +1,20 @@
+console.log('Trabalhando com Listas: ');
+
+const cidades = ['Salvador', 'São Paulo', 'Rio de Janeiro'];
+
+cidades.push('Curitiba'); // adiciona elemento
+
+console.log(cidades); // [ 'Salvador', 'São Paulo', 'Rio de Janeiro', 'Curitiba' ]
+
+cidades.splice(1, 1); // remove elemento na posição 1
+
+console.log('Destinos possíveis: ');
+
+console.log(cidades); // [ 'Salvador', 'Rio de Janeiro', 'Curitiba' ]
+
+console.log(cidades[1], cidades[2]); // Rio de Janeiro Curitiba
+
+
+
+
+
