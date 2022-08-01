899ea58a43c263ade6f419de5d24f6e2ebdd20de
nely de faria <nelly.nf54@gmail.com>
2022-08-01 18:18:27 -0300

    Cria declaracao-variavel.js

---
+++ declaracao-variavel.js
@@ -0,0 +1,10 @@
+const idade = 26;
+
+console.log('idade'); // idade
+console.log(idade); // 26
+console.log(idade + 2); // 28
+console.log(idade - 2); // 24
+console.log(idade / 2); // 13
+
+const idadeSomada = idade + 2; 
+console.log(idadeSomada); // 28
