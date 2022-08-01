cdb9206913569965a43f47494e44c0c6c24b5929
nely de faria <nelly.nf54@gmail.com>
2022-08-01 18:20:25 -0300

    Cria operadores-logicos.js

---
+++ operadores-logicos.js
@@ -0,0 +1,8 @@
+console.log('Operadores Lógicos: ');
+
+const idade = 15;
+console.log(idade > 18); // false
+console.log(idade < 18); // true
+console.log(idade <= 18); // true
+console.log(idade >= 18); // false
+console.log(idade == 18); // false
